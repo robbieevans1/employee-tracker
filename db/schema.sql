73068a149f9441cd64fecf2d176b591ce5a5e7e5
robcabobb <robcabobb@gmail.com>
2022-03-01 22:55:31 -0500

    added connection file, schema, and seeds file

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS business_db;
+CREATE DATABASE business_db;
+USE business_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,37 @@
+INSERT INTO departments (name)
+VALUES 
+    ('Leadership'),
+    ('Sales'),
+    ('Human Resources'),
+    ('Technology'),
+    ('Customer Service');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+    ('CEO', 1000000.00, 1),
+    ('Sales Manager', 100000.00, 2),
+    ('Sales Representatice', 50000.00, 2),
+    ('HR Manager', 75000.00, 3),
+    ('HR Generalist', 50000.00, 3),
+    ('IT Manager', 100000.00, 4),
+    ('Software Engineer', 70000.00, 4),
+    ('Customer Service Manager', 50000.00, 5),
+    ('CSR', 30000.00, 5);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
+    ('Zoie', 'Rube', 1, null),
+    ('Merrit', 'Hylda', 2, 1),
+    ('Sam', 'Smith', 4, 1),
+    ('Jenny', 'Luck', 6, 1),
+    ('Tim', 'Franklin', 8, 1),
+    ('Vanessa', 'Betancourt', 3, 2),
+    ('Austin', 'Davis', 3, 2),
+    ('Mary', 'Robinson', 3, 2),
+    ('Luke', 'OBrien', 3, 2),
+    ('Christy', 'Edwina', 5, 3),
+    ('Chuck', 'Jones', 5, 3),
+    ('Caroline', 'Jordan', 7, 4),
+    ('Joel', 'Andrews', 9, 5),
+    ('Melisandre', 'Evans', 9, 5),
+    ('Rina', 'Schoolcraft', 9, 5);
