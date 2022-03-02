98d79794707850e63e67c56300232300a2346ec9
robcabobb <robcabobb@gmail.com>
2022-03-01 22:52:26 -0500

    added connection file

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: "business_db"
+  },
+  console.log("Connected to database!")  
+)
+
+module.exports = db
